1171ebd74f6af2f14e7c6d9454d5469e84ba7eab
Vikas Chaturvedi <vc7068318009@gmail.com>
2023-12-08 23:45:11 +0530

    hfjd

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -130,6 +130,7 @@ const About = () => {
                     <IoIosCheckmarkCircleOutline />
                   </span>{" "}
                   Adaptability
+                  {/* done */}
                 </p>{" "}
               </div>
               <div>

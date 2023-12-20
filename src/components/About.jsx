4b9e9a0bbc72a0ae83160ceca9fb62b128687a28
Vikas Chaturvedi <vc7068318009@gmail.com>
2023-12-20 12:03:59 +0530

    check

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect } from "react";
-import resume from "../resume/VikasChaturvedi-resume.pdf"
+import resumePDF from "../resume/VikasChaturvedi-resume.pdf"; // Assuming correct path to resume PDF
 import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS CSS if not already imported
 
 export default function About() {
-
-  const handleDownload = () => {
-    window.open("https://drive.google.com/file/d/1cjd_bUS4TM2Z9_SRxF38IZvHmohPcOwV/view?usp=sharing", '_blank');
-  };
-
-  //hello world
-  // <h1></h1>
-
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
+
+  const handleDownload = () => {
+    window.open(
+      "https://drive.google.com/file/d/1cjd_bUS4TM2Z9_SRxF38IZvHmohPcOwV/view?usp=sharing",
+      "_blank"
+    );
+  };
 
   return (
     <section data-aos="fade-up" className="about section sections" id="about">
@@ -22,23 +22,23 @@ export default function About() {
         <span className="sauce">About Me</span>
       </h1>
       <div>
-        <div data-aos="fade-up" className=" first">
+        <div data-aos="fade-up" className="first">
           <p id="user-detail-intro">
-          Welcome to my coding realm, where I transform coffee into lines of code! <p style={{ display: 'inline-block' }} className="sauce">Me,vikas</p>
-          ,a proficient full stack web developer. My expertise spans across HTML5, CSS3, JavaScript, ReactJs, NodeJs, Express, and I'm well-versed in working with database 
-          technologies like MongoDB. My true passion lies in crafting websites that not only boast impressive aesthetics but also elicit 
-          exclamations of delight from users. I strive for excellence and pay meticulous attention to detail, enabling me to conquer even 
-          the most intricate coding challenges. When I'm not engrossed in coding, I love immersing myself in the latest tech trends and 
-          exploring new possibilities in the digital world..
-          
-            <br />
+            Welcome to my coding realm, where I transform coffee into lines of code!{" "}
+            <span style={{ display: 'inline-block' }} className="sauce">Me, Vikas</span>,
+            a proficient full stack web developer. My expertise spans across HTML5, CSS3, JavaScript, ReactJs, NodeJs, Express, and I'm well-versed in working with database technologies like MongoDB. My true passion lies in crafting websites that not only boast impressive aesthetics but also elicit exclamations of delight from users. I strive for excellence and pay meticulous attention to detail, enabling me to conquer even the most intricate coding challenges. When I'm not engrossed in coding, I love immersing myself in the latest tech trends and exploring new possibilities in the digital world.
           </p>
-          <br />
         </div>
-        <a 
-        onClick={handleDownload} 
-        rel="noreferrer" href={resume} download={"VikasChaturvedi_Resume"} id="resume-button-2" className="contact-button resume-button second-btn">
-          Resume</a>
+        <a
+          onClick={handleDownload}
+          rel="noreferrer"
+          href={resumePDF}
+          download={"VikasChaturvedi_Resume.pdf"} // Adjust the filename for download
+          id="resume-button-2"
+          className="contact-button resume-button second-btn"
+        >
+          Resume
+        </a>
       </div>
     </section>
   );
